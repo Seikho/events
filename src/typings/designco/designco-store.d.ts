38efa502f7db13df37ee380e48dda1044effd2d7
Carl <carl@carlossus.com>
2015-06-11 22:48:12 +0800

    removed massive redundancy from type defintions

--- src/typings/designco/designco-store.d.ts
+++ src/typings/designco/designco-store.d.ts
@@ -1,39 +1,20 @@
 declare module "designco-store" {
-	function pub(event: AppEvent);
-	function psub(channel: string, callback: (channel: string, pattern: string, message: string) => void);
-	function sub(channel: string, callback: (channel: string, message: string) => void);
-	function fetch(pattern: string, count?: number);
-	function setHost(hostname: string, port?: number);
-	
-	const enum EventType {
-		Create,
-		Read,
-		Update,
-		Delete,
-		Notification
-	}
-
-	export interface AppEvent {
-		event: EventType;
-		context: EventContext;
-		data: any;
-		key: string|number;
-	}
-
-	export const enum EventContext {
-		User,
-		Order,
-		Item
-	}
+	export = Store;
 }
 
 declare module Store {
+	export function pub(event: AppEvent);
+	export function psub(channel: string, callback: (channel: string, pattern: string, message: string) => void);
+	export function sub(channel: string, callback: (channel: string, message: string) => void);
+	export function fetch(pattern: string, count?: number);
+	export function setHost(hostname: string, port?: number);
+
 	export interface StoreApi {
-		config(key: string, value?: string): string;
 		pub(event: AppEvent);
 		psub(channel: string, callback: (channel: string, pattern: string, message: string) => void);
 		sub(channel: string, callback: (channel: string, message: string) => void);
 		fetch(pattern: string, count?: number);
+		setHost(hostname: string, port?: number);
 	}
 
 	export const enum EventType {
