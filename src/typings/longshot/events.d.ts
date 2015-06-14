4949b6e8f907a09d1e7397e19d5bf478aef95702
Carl <carl@carlossus.com>
2015-06-14 22:11:24 +0800

    Added 'client' to type definitions (external ambient module)

--- src/typings/longshot/events.d.ts
+++ src/typings/longshot/events.d.ts
@@ -1,5 +1,10 @@
 /// <reference path='../bluebird/bluebird.d.ts' />
+/// <reference path='../redis/redis.d.ts' />
+
 declare module "ls-events" {
+	import redis = require("redis");
+	
+	export function client(): redis.RedisClient;
 	export function pub(event: Event): Promise<{}>;
 	export function psub(channel: string, callback: (channel: string, pattern: string, message: string) => void): Promise<{}>;
 	export function sub(channel: string, callback: (channel: string, message: string) => void): Promise<{}>;
