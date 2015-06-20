c74cb8b927c18d08875399858c2611f0031e2d3d
Carl <carl@carlossus.com>
2015-06-20 12:51:38 +0800

    type defs: psub callback parameter ordering fixed

--- src/typings/longshot/config.d.ts
+++ src/typings/longshot/config.d.ts
@@ -1,3 +1,9 @@
+declare module NodeJS {
+	export interface Global {
+		longshotConfig: any;
+	}
+}
+
 declare module "ls-config" {
 	export function config(key: string, value?: any): any;
 }
--- src/typings/longshot/events.d.ts
+++ src/typings/longshot/events.d.ts
@@ -6,7 +6,7 @@ declare module "ls-events" {
 	
 	export function client(): redis.RedisClient;
 	export function pub(event: Event): Promise<{}>;
-	export function psub(channel: string, callback: (channel: string, pattern: string, message: string) => void): Promise<{}>;
+	export function psub(channel: string, callback: (pattern: string, channel: string, message: string) => void): Promise<{}>;
 	export function sub(channel: string, callback: (channel: string, message: string) => void): Promise<{}>;
 	export function fetch(context?: string, event?: string, key?: string): Promise<any>;
 	export function setHost(hostname: string, port?: number);
