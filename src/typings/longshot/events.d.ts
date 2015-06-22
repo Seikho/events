5c52e2ae7d331fd051c87dbcded71cd99f713ae2
Carl <carl@carlossus.com>
2015-06-22 20:04:51 +0800

    reverted type defintions. messages are indeed strings

--- src/typings/longshot/events.d.ts
+++ src/typings/longshot/events.d.ts
@@ -6,7 +6,7 @@ declare module "ls-events" {
 
 	export function client(): redis.RedisClient;
 	export function pub(event: Event): Promise<{}>;
-	export function psub(channel: string, callback: (pattern: string, channel: string, message: EventData) => void): Promise<{}>;
+	export function psub(channel: string, callback: (pattern: string, channel: string, message: string) => void): Promise<{}>;
 	export function sub(channel: string, callback: (channel: string, message: string) => void): Promise<{}>;
 	export function fetch(context?: string, event?: string, key?: string): Promise<any>;
 	export function setHost(hostname: string, port?: number);
@@ -16,10 +16,5 @@ declare module "ls-events" {
 		context: string;
 		data: any;
 		key: string|number;
-	}
-
-	export interface EventData {
-		published: number;
-		data: any;
 	}
 }
